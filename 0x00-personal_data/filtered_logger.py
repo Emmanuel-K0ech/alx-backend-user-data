2cca3efb5576f7c23a20dcdf7d88cd075923ef3e
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-11-07 18:25:45 +0300

    Task 0: Deal with message as a string not a list

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,7 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[int], redaction: str, message: str,
+def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> List[str]:
     """
     Returns the log message obfuscated
@@ -16,10 +16,9 @@ def filter_datum(fields: List[int], redaction: str, message: str,
                         seperating all fields in the log line
     Uses regex to replace occurrences of certain field values
     use re.sub to perform the substitutuion with a single regex
-    """
     # Text is seperated by ;, hence regex will start from '=' to ';'
-    new_fields: List[str] = []
+    """
     for field in fields:
-        pattern: str = fr"{re.escape(field)}=.*?(?={re.escape(separator)}|$)"
-        new_fields.append(re.sub(pattern, f"{field}={redaction}", message))
-    return new_fields
+        pattern = r"({}=)([^{}]+)".format(field, separator)
+        message = re.sub(pattern, r"\1" + redaction, message)
+    return message
