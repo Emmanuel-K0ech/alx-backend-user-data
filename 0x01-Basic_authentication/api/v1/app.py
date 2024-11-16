94b60ae2cdfbe9ac4cceea7ed87f587d71e02c8c
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-11-16 18:32:05 +0300

    Task 3: Request validation - implementing the environment variable statement

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,7 +6,6 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-from api.v1.auth.auth import Auth
 import os
 
 
@@ -14,8 +13,10 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+AUTH_TYPE = os.getenv('AUTH_TYPE')
 
-if auth:
+if AUTH_TYPE == "auth":
+    from api.v1.auth.auth import Auth
     auth = Auth()
 
 
@@ -45,7 +46,7 @@ def do_before_request():
         return
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
                       '/api/v1/forbidden/']
-    if auth.require_auth(request.path, exluded_paths):
+    if auth.require_auth(request.path, excluded_paths):
         if auth.authorization_header(request) is None:
             abort(401)
         elif auth.current_user(request) is None:
@@ -55,4 +56,4 @@ def do_before_request():
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(debug=True, host=host, port=port)
